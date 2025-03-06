ca8cd3bc69ce10100a1fa10e4485077b0b7d4b78
yonatanhb <92885822+yonatanhb@users.noreply.github.com>
2025-03-06 19:24:00 +0200

    Update Layout component; change background class for improved styling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
   const isLargeScreen = useMediaQuery({ minWidth: 1024 }); // lg breakpoint in Tailwind
 
   return isLargeScreen ? (
-    <div className="bg-white text-white min-h-screen">
+    <div className="bg-background text-white min-h-screen">
       <div className="max-w-[1736px] mx-auto px-0">{children}</div>
     </div>
   ) : (
