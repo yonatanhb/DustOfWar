dd3e38d94d00d2c2ca4884840078f80879f3a883
yonatanhb <92885822+yonatanhb@users.noreply.github.com>
2025-03-09 18:39:14 +0200

    Update Contact component; adjust button padding and font size for improved styling and responsiveness

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -113,7 +113,7 @@ const Contact = () => {
         <div className="flex justify-end">
           <button
             type="submit"
-            className="px-6 py-2 uppercase bg-highlight text-black font-chivo hover:bg-white transition-colors"
+            className="px-4 py-2 text-[14px] leading-[16px] sm:text-[16px] sm:leading-[18px] uppercase bg-highlight text-black font-chivo hover:bg-white transition-colors rounded-btn"
           >
             {loading ? "Sending..." : "Submit"}
           </button>
